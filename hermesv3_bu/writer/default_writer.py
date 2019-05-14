Corrected aleatory bug when setting dataframe into empty numpy array. Now is setting into zero numpy array Default writer: added long name and units

hermesv3_bu/writer/default_writer.py
@@ -113,12 +113,15 @@ class DefaultWriter(Writer):
             # var = netcdf.createVariable(var_name, np.float64, ('time', 'lev',) + var_dim,
             #                             chunksizes=self.rank_distribution[0]['shape'])
             var = netcdf.createVariable(var_name, np.float64, ('time', 'lev',) + var_dim)
-            var[:, :,
-            self.rank_distribution[self.comm_write.Get_rank()]['y_min']:
-            self.rank_distribution[self.comm_write.Get_rank()]['y_max'],
-            self.rank_distribution[self.comm_write.Get_rank()]['x_min']:
-            self.rank_distribution[self.comm_write.Get_rank()]['x_max']] = var_data
 
+            var[:, :,
+                self.rank_distribution[self.comm_write.Get_rank()]['y_min']:
+                self.rank_distribution[self.comm_write.Get_rank()]['y_max'],
+                self.rank_distribution[self.comm_write.Get_rank()]['x_min']:
+                self.rank_distribution[self.comm_write.Get_rank()]['x_max']] = var_data
+
+            var.long_name = self.pollutant_info.loc[var_name, 'description']
+            var.units = self.pollutant_info.loc[var_name, 'units']
             var.missing_value = -999.0
             var.coordinates = 'lat lon'
             if self.grid.grid_type == 'Regular Lat-Lon':

hermesv3_bu/writer/writer.py
@@ -113,14 +113,14 @@ class Writer(object):
         return new_emissions
 
     def dataframe_to_array(self, dataframe):
-        var_name = dataframe.columns.values
+        var_name = dataframe.columns.values[0]
         shape = self.rank_distribution[self.comm_write.Get_rank()]['shape']
         dataframe.reset_index(inplace=True)
         dataframe['FID'] = dataframe['FID'] - self.rank_distribution[self.comm_write.Get_rank()]['fid_min']
-        data = np.empty((shape[0], shape[1], shape[2] * shape[3]))
+        data = np.zeros((shape[0], shape[1], shape[2] * shape[3]))
 
         for (layer, tstep), aux_df in dataframe.groupby(['layer', 'tstep']):
-            data[tstep, layer, aux_df['FID']] = aux_df[var_name].values.flatten()
+            data[tstep, layer, aux_df['FID']] = aux_df[var_name]
         return data.reshape(shape)
 
     def write(self, emissions):