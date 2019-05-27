6ee4134565c8e6f15ca5d123f7387c9b440f474d
Carles Tena <carles.tena@bsc.es>
2019-05-27 14:56:58 +0200

    Added splitted communicator to sector

--- hermesv3_bu/sectors/sector_manager.py
+++ hermesv3_bu/sectors/sector_manager.py
@@ -6,31 +6,46 @@ SECTOR_LIST = ['traffic', 'traffic_area', 'aviation', 'point_sources', 'recreati
 
 class SectorManager(object):
     def __init__(self, comm_world, grid, clip, date_array, arguments):
-        self.sector_list = self.make_sector_list(arguments)
+        self.sector_list = self.make_sector_list(arguments, comm_world.Get_size())
 
-        if comm_world.Get_size() != sum(self.sector_list.values()):
-            raise ValueError("The selected number of processors '{0}' does not fit ".format(comm_world.Get_size()) +
-                             "with the sum of processors dedicated for all the sectors " +
-                             "'{0}': {1}".format(sum(self.sector_list.values()), self.sector_list))
-        from hermesv3_bu.sectors.aviation_sector import AviationSector
-        self.sector = AviationSector(comm_world, arguments.auxiliary_files_path, grid.shapefile, clip, date_array,
-                                     arguments.aviation_source_pollutants, grid.vertical_desctiption,
-                                     arguments.airport_list, arguments.plane_list, arguments.airport_shapefile_path,
-                                     arguments.airport_runways_shapefile_path,
-                                     arguments.airport_runways_corners_shapefile_path,
-                                     arguments.airport_trajectories_shapefile_path, arguments.airport_operations_path,
-                                     arguments.planes_path, arguments.airport_times_path, arguments.airport_ef_dir,
-                                     arguments.aviation_weekly_profiles, arguments.aviation_hourly_profiles,
-                                     arguments.speciation_map, arguments.aviation_speciation_profiles,
-                                     arguments.molecular_weights)
+        color = 10
+        for sector, sector_procs in self.sector_list.iteritems():
+            if sector == 'aviation' and comm_world.Get_rank() in sector_procs:
+                print '{0} -> {1}, {2}'.format(comm_world.Get_rank(), sector, sector_procs)
+                exit()
+                from hermesv3_bu.sectors.aviation_sector import AviationSector
+                self.sector = AviationSector(comm_world.Split(color, comm_world.Get_rank() - sector_procs[0]),
+                                             arguments.auxiliary_files_path, grid.shapefile, clip,
+                                             date_array, arguments.aviation_source_pollutants,
+                                             grid.vertical_desctiption, arguments.airport_list, arguments.plane_list,
+                                             arguments.airport_shapefile_path, arguments.airport_runways_shapefile_path,
+                                             arguments.airport_runways_corners_shapefile_path,
+                                             arguments.airport_trajectories_shapefile_path,
+                                             arguments.airport_operations_path, arguments.planes_path,
+                                             arguments.airport_times_path, arguments.airport_ef_dir,
+                                             arguments.aviation_weekly_profiles, arguments.aviation_hourly_profiles,
+                                             arguments.speciation_map, arguments.aviation_speciation_profiles,
+                                             arguments.molecular_weights)
+            elif sector == 'shipping_port' and comm_world.Get_rank() in sector_procs:
+                print '{0} -> {1}, {2}'.format(comm_world.Get_rank(), sector, sector_procs)
+                exit()
+            color += 1
 
     def run(self):
         return self.sector.calculate_emissions()
 
     @staticmethod
-    def make_sector_list(arguments):
+    def make_sector_list(arguments, max_procs):
         sector_dict = {}
+        accum = 0
         for sector in SECTOR_LIST:
             if vars(arguments)['do_{0}'.format(sector)]:
-                sector_dict[sector] = vars(arguments)['{0}_processors'.format(sector)]
+                n_procs = vars(arguments)['{0}_processors'.format(sector)]
+                sector_dict[sector] = [accum + x for x in xrange(n_procs)]
+                accum += n_procs
+        if accum != max_procs:
+            raise ValueError("The selected number of processors '{0}' does not fit ".format(max_procs) +
+                             "with the sum of processors dedicated for all the sectors " +
+                             "'{0}': {1}".format(accum, {sector: len(sector_procs)
+                                                         for sector, sector_procs in sector_dict.iteritems()}))
         return sector_dict
