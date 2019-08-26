fb38653760e6897f465b3c55ff7649607ac4c720
Carles Tena <carles.tena@bsc.es>
2019-08-26 12:55:02 +0200

    -Traffic area: Corrected parallel bug

--- hermesv3_bu/sectors/traffic_area_sector.py
+++ hermesv3_bu/sectors/traffic_area_sector.py
@@ -275,7 +275,7 @@ class TrafficAreaSector(Sector):
                                                                df1=self.evaporative, df2=temperature_mean,
                                                                geom1_col='centroid', src_column='REC', axis=1)
             del self.evaporative['c_lat'], self.evaporative['c_lon'], self.evaporative['centroid']
-            IoShapefile(self.comm).write_shapefile_serial(
+            IoShapefile(self.comm).write_shapefile_parallel(
                 self.evaporative, os.path.join(self.auxiliary_dir, 'traffic_area', 'vehicle_by_cell.shp'))
         else:
             del self.evaporative['c_lat'], self.evaporative['c_lon'], self.evaporative['centroid']
