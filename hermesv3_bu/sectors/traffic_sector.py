a231faea02ac79262aefc05949299e2121137469
ctena <carles.tena@bsc.es>
2021-09-03 14:21:27 +0200

    WIP

--- hermesv3_bu/sectors/traffic_sector.py
+++ hermesv3_bu/sectors/traffic_sector.py
@@ -182,8 +182,8 @@ class TrafficSector(Sector):
             scenario = gpd.sjoin(GeoDataFrame(index=self.road_links.index, geometry=self.road_links.geometry.centroid,
                                               crs=self.road_links.crs), scenario_shp, how='left')
             scenario = scenario.loc[:, scenario_shp.columns]
-            scenario.index.name = 'Link_ID'
-            print(scenario)
+            scenario = pd.DataFrame(scenario.drop(columns='geometry'), index=pd.Index(scenario.index, name='Link_ID'))
+
             scenario.fillna(1, inplace=True)
 
         else:
